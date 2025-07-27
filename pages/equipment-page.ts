95317b8ec173807d0892e57a9e9dfa19ec73f68e
Butch Mayhew <bmayhew@tilled.com>
2025-07-26 20:47:53 -0500

    adding awaits

--- pages/equipment-page.ts
+++ pages/equipment-page.ts
@@ -1,71 +1,76 @@
-import { type Locator, type Page, expect } from '@playwright/test';
+import { type Locator, type Page, expect } from "@playwright/test";
 
 export class EquipmentPage {
-    readonly page: Page;
-    readonly errorMessage: Locator;
-    readonly initialFuelField: Locator;
-    readonly numMissilesDropdown: Locator;
-    readonly numDumbBombDropdown: Locator;
-    readonly reconPodCheckbox: Locator;
-    readonly intelliBombCheckbox: Locator;
-    readonly fuelTankCheckbox: Locator;
-    readonly loadButton: Locator;
-    readonly restartButton: Locator;
-    readonly loadingEquipmentIndicator: Locator;
-    readonly errorMessageForFuelWithoutNoTank = 'Max fuel level of 3000kg';
-    readonly errorMessageForFuelWithExtTank   = 'Max fuel level of 6000kg';
+  readonly page: Page;
+  readonly errorMessage: Locator;
+  readonly initialFuelField: Locator;
+  readonly numMissilesDropdown: Locator;
+  readonly numDumbBombDropdown: Locator;
+  readonly reconPodCheckbox: Locator;
+  readonly intelliBombCheckbox: Locator;
+  readonly fuelTankCheckbox: Locator;
+  readonly loadButton: Locator;
+  readonly restartButton: Locator;
+  readonly loadingEquipmentIndicator: Locator;
+  readonly errorMessageForFuelWithoutNoTank = "Max fuel level of 3000kg";
+  readonly errorMessageForFuelWithExtTank = "Max fuel level of 6000kg";
 
-    constructor(page: Page) {
-        this.page = page;
-        this.errorMessage = this.page.locator('#initErrorMsg');
-        this.initialFuelField = this.page.locator('#initialFuel');
-        this.numMissilesDropdown = this.page.getByLabel('Number of missiles');
-        this.numDumbBombDropdown = this.page.getByLabel('Number of dumb bombs');
-        this.reconPodCheckbox = this.page.locator('#reconPod');
-        this.intelliBombCheckbox =this.page.locator('#intelliBomb');
-        this.fuelTankCheckbox = this.page.locator('#fuelTank');
-        // There is a reason I've selected LoadButton slightly differently to restart for testing other builds
-        //this.loadButton = this.page.getByRole('button', { name: 'Load'});
-        this.loadButton = this.page.locator('#setEquipmentButton');
-        this.restartButton = this.page.getByRole('button', { name: 'Restart'});
-        this.loadingEquipmentIndicator = this.page.locator('#loadEquipment');
+  constructor(page: Page) {
+    this.page = page;
+    this.errorMessage = this.page.locator("#initErrorMsg");
+    this.initialFuelField = this.page.locator("#initialFuel");
+    this.numMissilesDropdown = this.page.getByLabel("Number of missiles");
+    this.numDumbBombDropdown = this.page.getByLabel("Number of dumb bombs");
+    this.reconPodCheckbox = this.page.locator("#reconPod");
+    this.intelliBombCheckbox = this.page.locator("#intelliBomb");
+    this.fuelTankCheckbox = this.page.locator("#fuelTank");
+    // There is a reason I've selected LoadButton slightly differently to restart for testing other builds
+    //this.loadButton = this.page.getByRole('button', { name: 'Load'});
+    this.loadButton = this.page.locator("#setEquipmentButton");
+    this.restartButton = this.page.getByRole("button", { name: "Restart" });
+    this.loadingEquipmentIndicator = this.page.locator("#loadEquipment");
+  }
 
+  async setFuel(inputString) {
+    //await this.initialFuelField.clear();
+    await this.initialFuelField.click();
+    await this.initialFuelField.fill("");
+    await this.initialFuelField.fill(inputString);
+  }
+
+  async setMissileNum(numMissiles) {
+    await this.numMissilesDropdown.selectOption(numMissiles);
+  }
+
+  async setDumbBombNum(numBomb) {
+    await this.numDumbBombDropdown.selectOption(numBomb);
+  }
+
+  async setAircraftEquipment(
+    fuel,
+    numMissiles,
+    numDumbBomb,
+    reconPod,
+    intelliBomb,
+    fuelTank
+  ) {
+    await this.setFuel(fuel);
+    await this.setMissileNum(numMissiles);
+    await this.setDumbBombNum(numDumbBomb);
+    if (reconPod) {
+      await this.reconPodCheckbox.check();
     }
-
-    async setFuel(inputString) {
-        //await this.initialFuelField.clear();
-        await this.initialFuelField.click();
-        await this.initialFuelField.fill('');
-        await this.initialFuelField.fill(inputString);
+    if (intelliBomb) {
+      await this.intelliBombCheckbox.check();
     }
-
-    async setMissileNum(numMissiles) {
-        await this.numMissilesDropdown.selectOption(numMissiles);
+    if (fuelTank) {
+      await this.fuelTankCheckbox.check();
     }
+  }
 
-    async setDumbBombNum(numBomb) {
-        await this.numDumbBombDropdown.selectOption(numBomb);
-    }
-
-    async setAircraftEquipment (fuel, numMissiles, numDumbBomb, reconPod, intelliBomb, fuelTank ) {
-        this.setFuel(fuel);
-        this.setMissileNum(numMissiles);
-        this.setDumbBombNum(numDumbBomb);
-        if( reconPod ) {
-            this.reconPodCheckbox.check();
-        }
-        if( intelliBomb ) {
-            this.intelliBombCheckbox.check();
-        }
-        if( fuelTank ) {
-            this.fuelTankCheckbox.check();
-        }
-    }
-
-    async setDefaultEquipment (fuel) {
-        this.setAircraftEquipment( fuel, 6, 4, false, false, false );
-    }
-
+  async setDefaultEquipment(fuel) {
+    this.setAircraftEquipment(fuel, 6, 4, false, false, false);
+  }
 }
 
 export default EquipmentPage;
--- tests/panther-statuspage.spec.ts
+++ tests/panther-statuspage.spec.ts
@@ -1,93 +1,108 @@
-import { test, expect } from '@playwright/test';
-import { BuildPage } from '../pages/build-page';
-import { EquipmentPage } from '../pages/equipment-page';
-import { StatusPage } from '../pages/status-page';
-import { Helper } from '../helper/helper';
+import { test, expect } from "@playwright/test";
+import { BuildPage } from "../pages/build-page";
+import { EquipmentPage } from "../pages/equipment-page";
+import { StatusPage } from "../pages/status-page";
+import { Helper } from "../helper/helper";
 
-const URL = 'https://testsheepnz.github.io/panther.html';
+const URL = "https://testsheepnz.github.io/panther.html";
 let helper: Helper;
 let buildPage: BuildPage;
 let equipmentPage: EquipmentPage;
 let statusPage: StatusPage;
 
-test.beforeEach(async ({page}) => {
-    buildPage = new BuildPage(page);
-    await buildPage.goToBuildPage();
-    await buildPage.openApplicationPageUnderTest();
-    equipmentPage = new EquipmentPage(page);
-    equipmentPage.setFuel('1000');
-    statusPage = new StatusPage(page);
+test.beforeEach(async ({ page }) => {
+  buildPage = new BuildPage(page);
+  await buildPage.goToBuildPage();
+  await buildPage.openApplicationPageUnderTest();
+  equipmentPage = new EquipmentPage(page);
+  await equipmentPage.setFuel("1000");
+  statusPage = new StatusPage(page);
 });
 
 /* Check visible elements on startup */
-test('Aircraft leg is visible', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftLegField).toBeVisible();
+test("Aircraft leg is visible", async ({ page }) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftLegField).toBeVisible();
 });
 
-test('Aircraft altitude is visible', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftAltitudeField).toBeVisible();
+test("Aircraft altitude is visible", async ({ page }) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftAltitudeField).toBeVisible();
 });
 
-test('Aircraft weight is visible', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftWeightField).toBeVisible();
+test("Aircraft weight is visible", async ({ page }) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftWeightField).toBeVisible();
 });
 
-test('Aircraft fuel field is visible', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftFuelRemainingField).toBeVisible();
+test("Aircraft fuel field is visible", async ({ page }) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftFuelRemainingField).toBeVisible();
 });
 
-test('Aircraft fuel used in last leg is visible', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftFuelRemainingField).toBeVisible();
+test("Aircraft fuel used in last leg is visible", async ({ page }) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftFuelRemainingField).toBeVisible();
 });
 
-test('Aircraft action dropdown is visible', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftActionDropdown).toBeVisible();
+test("Aircraft action dropdown is visible", async ({ page }) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftActionDropdown).toBeVisible();
 });
 
-test('Restart takes back to equipment section', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftLegField).toBeVisible();
-    await equipmentPage.restartButton.click();
-    await expect(statusPage.aircraftLegField).toBeHidden();
-    await expect(equipmentPage.initialFuelField).toBeVisible();
-    await expect(equipmentPage.loadButton).toBeVisible();
+test("Restart takes back to equipment section", async ({ page }) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftLegField).toBeVisible();
+  await equipmentPage.restartButton.click();
+  await expect(statusPage.aircraftLegField).toBeHidden();
+  await expect(equipmentPage.initialFuelField).toBeVisible();
+  await expect(equipmentPage.loadButton).toBeVisible();
 });
 
-test('After equipment load, aircraft leg is set to 0', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftLegField).toHaveValue('0');
+test("After equipment load, aircraft leg is set to 0", async ({ page }) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftLegField).toHaveValue("0");
 });
 
-test('After equipment load, aircraft altitude is set to 0', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftAltitudeField).toHaveValue('0');
+test("After equipment load, aircraft altitude is set to 0", async ({
+  page,
+}) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftAltitudeField).toHaveValue("0");
 });
 
-test('After equipment load, fuel used last leg is set to 0', async ({ page }) => {
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftFuelLastLeg).toHaveValue('0');
+test("After equipment load, fuel used last leg is set to 0", async ({
+  page,
+}) => {
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftFuelLastLeg).toHaveValue("0");
 });
 
-test('After entering fuel of 2145kg on equipment page, status page says 2145kg fuel available', async ({ page }) => {
-    const fuelVal = '2145';
-    await equipmentPage.setFuel(fuelVal);
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftFuelRemainingField).toBeVisible(); 
-    await expect(statusPage.aircraftFuelRemainingField).toHaveValue(fuelVal);
+test("After entering fuel of 2145kg on equipment page, status page says 2145kg fuel available", async ({
+  page,
+}) => {
+  const fuelVal = "2145";
+  await equipmentPage.setFuel(fuelVal);
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftFuelRemainingField).toBeVisible();
+  await expect(statusPage.aircraftFuelRemainingField).toHaveValue(fuelVal);
 });
 
 // Tests where we check the data provided
 
-test('Aircraft has an all up weight of 17000 if loaded with 6000kg fuel, 8 missiles, 6 dumb bombs, ext fuel tank', async ({ page }) => {
-    const expectedAllUpWeight = '17000';
-    await equipmentPage.setAircraftEquipment('6000', '8', '6', false, false, true);
-    await equipmentPage.loadButton.click();
-    await expect(statusPage.aircraftWeightField).toBeVisible(); 
-    await expect(statusPage.aircraftWeightField).toHaveValue(expectedAllUpWeight);
+test("Aircraft has an all up weight of 17000 if loaded with 6000kg fuel, 8 missiles, 6 dumb bombs, ext fuel tank", async ({
+  page,
+}) => {
+  const expectedAllUpWeight = "17000";
+  await equipmentPage.setAircraftEquipment(
+    "6000",
+    "8",
+    "6",
+    false,
+    false,
+    true
+  );
+  await equipmentPage.loadButton.click();
+  await expect(statusPage.aircraftWeightField).toBeVisible();
+  await expect(statusPage.aircraftWeightField).toHaveValue(expectedAllUpWeight);
 });
